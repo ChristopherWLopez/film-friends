1184a12cbdb2de4494eb5da366a126fb439a3a9a
christopher lopez <christopherwlopez89@gmail.com>
2024-05-07 18:19:56 -0700

    banner

--- app/components/head/banner/page.tsx
+++ app/components/head/banner/page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Banner = () => {
-  return (
-    <div className='Banner'>Banner</div>
-  )
-}
+  return <div 
+  className="Banner 
+  font-serif 
+  text-8xl
+  indent-20
+  text-amber-700
+  box-border h-32s
+  mx-auto
+  mt-20
+  ml-80
+  mr-80
+  border
 
-export default Banner
+  ">Film Friends</div>;
+};
+
+export default Banner;
