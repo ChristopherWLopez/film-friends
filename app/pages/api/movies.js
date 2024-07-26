aea99611d0b16f0c34fac8312362a22f171e0b89
christopher lopez <christopherwlopez89@gmail.com>
2024-07-25 18:27:15 -0700

    gituations

--- app/pages/api/movies.js
+++ app/pages/api/movies.js
@@ -1,10 +1,8 @@
 export default async function handler(req, res){
     const API_KEY = process.env.TMB_API_KEY;
-<<<<<<< HEAD
+
     const response = await fetch(`http://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`);
-=======
-    const response = await fetch();
->>>>>>> eb9f3a0b0036a136fa6af651f30fe2f0c5379d12
+
     const data = await response.json();
 
     res.status(200).json(data);
