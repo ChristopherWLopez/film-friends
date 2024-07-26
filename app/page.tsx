eb9f3a0b0036a136fa6af651f30fe2f0c5379d12
christopher lopez <christopherwlopez89@gmail.com>
2024-07-25 17:57:44 -0700

    api route

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import Banner from "./components/head/banner/page";
 import NavBar from "./components/head/navBar/page";
 
 
+
 export default function Home() {
   return (
     <>
       <Banner/>
       <NavBar/>
+      <Home/>
       
     </>
   );
