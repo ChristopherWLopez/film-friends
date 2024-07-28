c3a663f9325c596888d2f8daff0d23096e20de27
christopher lopez <christopherwlopez89@gmail.com>
2024-07-27 18:34:39 -0700

    server

--- app/pages/api/movies.js
+++ app/pages/api/movies.js
@@ -1,9 +1,24 @@
+
 export default async function handler(req, res){
-    const API_KEY = process.env.TMB_API_KEY;
 
-    const response = await fetch(`http://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`);
+    if(req.method === 'GET'){
+        const apiKey = process.env.MOVIE_DB_API_KEY;
+        const apiUrl = `http://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`
 
-    const data = await response.json();
+        try{
+            const response = await fetch(apiUrl);
+            if(!response.ok){
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            res.status(200).json(data);
+        }catch(error){
+            console.error('Failed to fetch', error);
+            res.status(500).json({ error: 'Failed to fetch '})
+        }
+    }else{
+        res.setHeader('ALLOW, ['GET']);
+        res.status(405).end(`MTHOED${req.method}`)
+    }
 
-    res.status(200).json(data);
 }
